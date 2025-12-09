5c69e7df294fb0ad0b1ca328c0bc4dd8af0478a0
Filippo Wang <51003235+PhilHippo@users.noreply.github.com>
2025-12-09 12:20:08 +0100

    Skip Phase 1 optimization

--- src/proposer.py
+++ src/proposer.py
@@ -11,7 +11,6 @@ class Proposer:
         self.c_rnd = 0
         self.quorum_1B = []
         self.quorum_2B = []
-        self.skip = False
         self.r = mcast_receiver(config["proposers"])
         self.s = mcast_sender()
         self.value = None
@@ -22,13 +21,18 @@ class Proposer:
 
         # number of acceptors
         self.majority_acceptors = math.ceil(self.config["n"]/2)
+        
+        # Proactive prepare optimization: track if we have quorum but no value
+        self.has_proactive_quorum = False
+        self.proactive_instance_seq = None
 
-    def send_1A(self, msg_num, client_id):
+    def send_1A(self, msg_num=None, client_id=None):
         self.c_rnd += 1
-        self.skip = False
         # reset quorums
         self.quorum_1B = []
         self.quorum_2B = []
+        self.has_proactive_quorum = False
+        self.proactive_instance_seq = None
 
         msg_1A = pickle.dumps(["1A", self.c_rnd, self.id, msg_num, client_id])
         self.s.sendto(msg_1A, self.config["acceptors"])
@@ -50,7 +54,20 @@ class Proposer:
                     if not self.queue:
                         self.value = value
                         self.queue.append((msg_num, client_id, value))
-                        self.send_1A(msg_num, client_id)
+                        
+                        # Optimization: if we already have a proactive quorum, skip 1A and go directly to 2A
+                        if self.has_proactive_quorum and self.proactive_instance_seq is not None:
+                            # Use the instance_seq from the proactive prepare
+                            self.instance_seq = self.proactive_instance_seq
+                            c_val = self.value
+                            msg_2A = pickle.dumps(["2A", self.c_rnd, c_val, self.id, self.instance_seq, msg_num, client_id])
+                            self.s.sendto(msg_2A, self.config["acceptors"])
+                            logging.debug(f"Sending {pickle.loads(msg_2A)} to acceptors (proactive optimization)")
+                            # Reset proactive state
+                            self.has_proactive_quorum = False
+                            self.proactive_instance_seq = None
+                        else:
+                            self.send_1A(msg_num, client_id)
                     else:
                         self.queue.append((msg_num, client_id, value))
                         
@@ -73,14 +90,22 @@ class Proposer:
                                         max_inst = max_in_hist
                             
                             # 2. Update local sequence to be next available slot
-                            self.instance_seq = max(self.instance_seq, max_inst + 1)
-                            logging.debug(f"Proposing in instance: {self.instance_seq}")
-
-                            # 3. Propose current value
-                            c_val = self.value
-                            msg_2A = pickle.dumps(["2A", self.c_rnd, c_val, self.id, self.instance_seq, msg_num, client_id])
-                            self.s.sendto(msg_2A, self.config["acceptors"])
-                            logging.debug(f"Sending {pickle.loads(msg_2A)} to acceptors")
+                            next_instance = max(self.instance_seq, max_inst + 1)
+                            
+                            # 3. Check if we have a value to propose
+                            if self.value is not None:
+                                # We have a value, proceed with 2A
+                                self.instance_seq = next_instance
+                                logging.debug(f"Proposing in instance: {self.instance_seq}")
+                                c_val = self.value
+                                msg_2A = pickle.dumps(["2A", self.c_rnd, c_val, self.id, self.instance_seq, msg_num, client_id])
+                                self.s.sendto(msg_2A, self.config["acceptors"])
+                                logging.debug(f"Sending {pickle.loads(msg_2A)} to acceptors")
+                            else:
+                                # Optimization: We have quorum but no value yet - store for later
+                                self.has_proactive_quorum = True
+                                self.proactive_instance_seq = next_instance
+                                logging.debug(f"Proactive quorum ready for instance {next_instance}, waiting for value")
 
                 case "2B":
                     v_rnd, v_val, id, msg_num, client_id = msg[1:]
@@ -98,10 +123,17 @@ class Proposer:
 
                             logging.debug(f"popping queue: {self.queue[0]}")
                             self.queue.popleft()
+                            
+                            # Optimization: Send proactive 1A for next instance even if queue is empty
                             if self.queue:
                                 msg_num, client_id, value = self.queue[0]
                                 self.value = value
                                 self.send_1A(msg_num, client_id)
+                            else:
+                                # No value in queue, but send proactive 1A to prepare for future values
+                                self.value = None
+                                self.send_1A(None, None)
+                                logging.debug("Sent proactive 1A (no value in queue)")
                     
                 case _:
                     logging.error(f"Unknown message: {msg}")
